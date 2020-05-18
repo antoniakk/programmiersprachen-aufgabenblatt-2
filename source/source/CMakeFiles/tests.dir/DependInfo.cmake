2d86b9638da2ac0f853b3543490fa63df4fc690d
Antonia K <antonia.krumieva@gmail.com>
2020-05-18 18:29:14 +0300

    Create vec2.cpp and vec2.hpp, and update CMakeList.txt

---
+++ source/source/CMakeFiles/tests.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/toni/Documents/Bauhaus_Uni/Progammiersprachen2020/programmiersprachen-aufgabenblatt-2/source/glew.c" "/home/toni/Documents/Bauhaus_Uni/Progammiersprachen2020/programmiersprachen-aufgabenblatt-2/source/source/CMakeFiles/tests.dir/glew.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external"
+  "../external/glfw-3.2.1/include"
+  "../external/nanovg/src"
+  "../external/catch-1.1"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/toni/Documents/Bauhaus_Uni/Progammiersprachen2020/programmiersprachen-aufgabenblatt-2/source/tests.cpp" "/home/toni/Documents/Bauhaus_Uni/Progammiersprachen2020/programmiersprachen-aufgabenblatt-2/source/source/CMakeFiles/tests.dir/tests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external"
+  "../external/glfw-3.2.1/include"
+  "../external/nanovg/src"
+  "../external/catch-1.1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/toni/Documents/Bauhaus_Uni/Progammiersprachen2020/programmiersprachen-aufgabenblatt-2/source/external/glfw-3.2.1/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/toni/Documents/Bauhaus_Uni/Progammiersprachen2020/programmiersprachen-aufgabenblatt-2/source/CMakeFiles/nanovg.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
