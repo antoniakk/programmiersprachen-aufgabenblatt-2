6cbea5e57c6f4a9e81ca36d6983ac396de3307af
Antonia K <antonia.krumieva@gmail.com>
2020-05-20 18:28:10 +0300

    Create color.hpp

--- source/color.hpp
+++ source/color.hpp
@@ -1,11 +1,8 @@
-# ifndef COLOR_HPP
-# define COLOR_HPP
+#ifndef COLOR_HPP
+#define COLOR_HPP
 
 struct Color {
     float r = 0.5f, b = 0.5f; g = 0.5f;
 }
 
-
-
-
-# endif // COLOR_HPP
+#endif // COLOR_HPP
