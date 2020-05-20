b5778cc77a1e6d286d48e916dd0849e81dbec108
Antonia K <antonia.krumieva@gmail.com>
2020-05-20 12:55:44 +0300

    Add Vec2 definition

--- source/vec2.hpp
+++ source/vec2.hpp
@@ -5,5 +5,9 @@ struct Vec2
 {
 float x = 0.0f;
 float y = 0.0f;
+Vec2& operator+=(Vec2 const & v);
+Vec2& operator-=(Vec2 const & v);
+Vec2& operator*=(float s);
+Vec2& operator/=(float s);
 };
 # endif // VEC2_HPP
