1f28d394c347674ef818131d36b168880bbe79b1
Antonia K <antonia.krumieva@gmail.com>
2020-05-20 18:31:40 +0300

    Update tests.cpp

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,16 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.hpp"
+
+TEST_CASE("vector_initialization", "[vec]") {
+  Vec2 a;
+  REQUIRE(0.0f == a.x);
+  REQUIRE(0.0f == a.y);
+  Vec2 b{5.1f, -9.3f};
+  REQUIRE(5.1f == Approx(b.x));
+  REQUIRE(-9.3f == Approx(b.y));
+  a+=b;
+} 
 
 int main(int argc, char *argv[])
 {
