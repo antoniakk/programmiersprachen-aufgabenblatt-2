be169a09f900c704946f457f302474107f3587bc
Antonia K <antonia.krumieva@gmail.com>
2020-05-26 01:19:06 +0300

    Replace tmp floats with counters

--- source/analoguhr.cpp
+++ source/analoguhr.cpp
@@ -12,11 +12,12 @@ int main(int argc, char* argv[])
 {
   Window win{std::make_pair(800,800)};
   Circle clock{300.0, {400.0, 400.0}, {1.0, 1.0, 1.0}};
-  Vec2 seconds{0.0, 300.0};
-  Vec2 mins{0.0, 300.0};
-  Vec2 hrs{0.0, 300.0};
-  Mat2 rotation = make_rotation_mat2(-2*M_PI/60);
-  float tmp_sec = 0.0, tmp_mins = 0.0, tmp_hrs = 0.0;
+  Vec2 seconds{0.0, 280.0};
+  Vec2 mins{0.0, 250.0};
+  Vec2 hrs{0.0, 200.0};
+  Mat2 rotation = make_rotation_mat2(-M_PI/30);
+  float tmp_sec = 0.0;
+  unsigned int count_sec = 0, count_min = 0;
 
   while (!win.should_close()) {
     if (win.get_key(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -27,27 +28,27 @@ int main(int argc, char* argv[])
     auto time = win.get_time();
 
     if(time - tmp_sec >= 1) {
-    seconds = (rotation*seconds);
-    tmp_sec = time; 
+      seconds = rotation*seconds;
+      tmp_sec = time; 
+      ++count_sec;
     }
-    if(time - tmp_mins >= 60) {
-    mins = (rotation*mins); 
-    tmp_mins = time;
+    if(count_sec == 60) {
+      mins = rotation*mins;
+      count_sec = 0;
+      ++count_min;
     }
-    if(time - tmp_hrs >= 3600) {
-    hrs = (rotation*hrs); 
-    tmp_hrs = time;
+    if (count_min == 60) {
+      hrs = rotation*hrs;
+      count_min = 0;
     }
     
-
-    
-    win.draw_line(clock.get_origin().x, clock.get_origin().y, clock.get_origin().x + seconds.x, clock.get_origin().x - seconds.y, 0.0, 0.0, 1.0);
-    win.draw_line(clock.get_origin().x, clock.get_origin().y, clock.get_origin().x + mins.x, clock.get_origin().x - mins.y, 1.0, 0.0, 1.0);
-    win.draw_line(clock.get_origin().x, clock.get_origin().y, clock.get_origin().x + hrs.x, clock.get_origin().x - hrs.y, 0.6, 0.0, 0.5);
-    win.draw_text(400.0, 130.0, 50.0, "XII");
-    win.draw_text(130.0, 400.0, 50.0, "IX");
-    win.draw_text(400.0, 630.0, 50.0, "VI");
-    win.draw_text(630.0, 400.0, 50.0, "III");
+    win.draw_line(clock.get_origin().x, clock.get_origin().y, clock.get_origin().x + seconds.x, clock.get_origin().y - seconds.y, 0.0, 0.0, 2.0);
+    win.draw_line(clock.get_origin().x, clock.get_origin().y, clock.get_origin().x + mins.x, clock.get_origin().y - mins.y, 1.0, 0.0, 1.0, 3.0);
+    win.draw_line(clock.get_origin().x, clock.get_origin().y, clock.get_origin().x + hrs.x, clock.get_origin().y - hrs.y, 0.6, 0.0, 0.5, 4.0);
+    win.draw_text(375.0, 130.0, 50.0, "XII");
+    win.draw_text(130.0, 375.0, 50.0, "IX");
+    win.draw_text(375.0, 630.0, 50.0, "VI");
+    win.draw_text(630.0, 375.0, 50.0, "III");
 
     
     win.update();
